981e2095364cefe05136c14ee60ab483e6c10b0a
dimovat9 <dimovatanja9@gmail.com>
2024-04-09 07:40:23 +0200

    Fix Code smells

--- src/main/java/com/mhealth/userservice/service/UserProfileServiceImpl.java
+++ src/main/java/com/mhealth/userservice/service/UserProfileServiceImpl.java
@@ -2,7 +2,6 @@ package com.mhealth.userservice.service;
 
 import com.mhealth.userservice.entity.UserProfile;
 import com.mhealth.userservice.repository.UserProfileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,8 +9,11 @@ import java.util.Optional;
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
-    @Autowired
-    private UserProfileRepository userProfileRepository;
+    private final UserProfileRepository userProfileRepository;
+
+    public UserProfileServiceImpl(UserProfileRepository userProfileRepository) {
+        this.userProfileRepository = userProfileRepository;
+    }
 
     @Override
     public UserProfile createUserProfile(UserProfile userProfile) {
@@ -24,42 +26,17 @@ public class UserProfileServiceImpl implements UserProfileService {
         return userProfileOptional.orElse(null);
     }
 
-
     @Override
     public UserProfile updateUserProfile(UserProfile userProfile) {
         // Ensure the user profile has a non-null ID
         if (userProfile.getId() == null) {
             throw new IllegalArgumentException("User profile ID cannot be null");
         }
-
         // Get the existing user profile from the database
         Optional<UserProfile> userProfileOptional = userProfileRepository.findById(userProfile.getId());
         if (userProfileOptional.isPresent()) {
             UserProfile existingProfile = userProfileOptional.get();
-
-            // Update only the fields that are not null in the provided user profile
-            if (userProfile.getAge() != null) {
-                existingProfile.setAge(userProfile.getAge());
-            }
-            if (userProfile.getGender() != null) {
-                existingProfile.setGender(userProfile.getGender());
-            }
-            if (userProfile.getWeight() != null) {
-                existingProfile.setWeight(userProfile.getWeight());
-            }
-            if (userProfile.getHeight() != null) {
-                existingProfile.setHeight(userProfile.getHeight());
-            }
-            if (userProfile.getActivityLevel() != null) {
-                existingProfile.setActivityLevel(userProfile.getActivityLevel());
-            }
-            if (userProfile.getDietaryPreferences() != null) {
-                existingProfile.setDietaryPreferences(userProfile.getDietaryPreferences());
-            }
-            if (userProfile.getFitnessGoals() != null) {
-                existingProfile.setFitnessGoals(userProfile.getFitnessGoals());
-            }
-
+            updateFields(existingProfile, userProfile);
             // Save the updated user profile
             return userProfileRepository.save(existingProfile);
         } else {
@@ -68,11 +45,32 @@ public class UserProfileServiceImpl implements UserProfileService {
         }
     }
 
+    private void updateFields(UserProfile existingProfile, UserProfile updatedProfile) {
+        if (updatedProfile.getAge() != null) {
+            existingProfile.setAge(updatedProfile.getAge());
+        }
+        if (updatedProfile.getGender() != null) {
+            existingProfile.setGender(updatedProfile.getGender());
+        }
+        if (updatedProfile.getWeight() != null) {
+            existingProfile.setWeight(updatedProfile.getWeight());
+        }
+        if (updatedProfile.getHeight() != null) {
+            existingProfile.setHeight(updatedProfile.getHeight());
+        }
+        if (updatedProfile.getActivityLevel() != null) {
+            existingProfile.setActivityLevel(updatedProfile.getActivityLevel());
+        }
+        if (updatedProfile.getDietaryPreferences() != null) {
+            existingProfile.setDietaryPreferences(updatedProfile.getDietaryPreferences());
+        }
+        if (updatedProfile.getFitnessGoals() != null) {
+            existingProfile.setFitnessGoals(updatedProfile.getFitnessGoals());
+        }
+    }
 
     @Override
     public void deleteUserProfile(Long userProfileId) {
         userProfileRepository.deleteById(userProfileId);
     }
-
-
 }
