99c364ec82232ad254f027a5abd5c0bc1f1919aa
dimovat9 <dimovatanja9@gmail.com>
2024-10-05 20:32:48 +0200

    users

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -43,9 +43,11 @@ def create_app():
     def disable_csrf_for_json_requests():
         if request.content_type == 'application/json':
             app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF for JSON requests
+            app.config['WTF_CSRF_METHODS'] = []  # This is the magic
             csrf.exempt(request.endpoint)
         if request.headers.get('X-Internal-Request', False):
             app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF for internal requests
+            app.config['WTF_CSRF_METHODS'] = []  # This is the magic
 
     # Initialize extensions with the app
     db.init_app(app)
