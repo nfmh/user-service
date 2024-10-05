4b2f2946e2b22bd240105283979c7999262a6d8b
dimovat9 <dimovatanja9@gmail.com>
2024-10-05 20:23:14 +0200

    users

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask_wtf.csrf import CSRFProtect, csrf_exempt
+from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 import os
 from flask import request
