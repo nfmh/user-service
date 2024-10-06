b50faddc28fc110c51ce3e8fd2981c336174a7cd
dimovat9 <dimovatanja9@gmail.com>
2024-10-06 17:44:33 +0200

    disable secure cookie

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     app.config['JWT_TOKEN_LOCATION'] = ['cookies']
     app.config['JWT_COOKIE_CSRF_PROTECT'] = True
     app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
-    app.config['JWT_COOKIE_SECURE'] = True
+    app.config['JWT_COOKIE_SECURE'] = False
     app.config['JWT_COOKIE_SAMESITE'] = 'Lax'
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
