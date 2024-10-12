1acfd1edf7e4d3cc1c96e88c00da426746f50117
dimovat9 <dimovatanja9@gmail.com>
2024-10-12 23:53:18 +0200

    allowed origins

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -21,9 +21,9 @@ def create_app():
     # Application configurations
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  
     app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-    app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False
     app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
-    app.config['JWT_COOKIE_SECURE']  = os.getenv('FLASK_ENV') == 'production'
+    app.config['JWT_COOKIE_SECURE']  = False
     app.config['JWT_COOKIE_SAMESITE'] = 'None'  # Allows cross-origin usage while keeping security
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -43,7 +43,8 @@ def create_app():
     def get_csrf_token():
         token = generate_csrf()
         response = make_response({'csrf_token': token})
-        response.set_cookie('csrf_token', token, httponly=False, secure=True, samesite='None')  # Ensure CSRF token is sent with SameSite=None
+        response.set_cookie('csrf_token', token, httponly=False, secure=True, samesite='None')
+        #response.set_cookie('csrf_token', token)  # Ensure CSRF token is sent with SameSite=None
         return response
 
     # Initialize extensions with the app
